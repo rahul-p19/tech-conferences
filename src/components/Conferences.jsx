a3470c0796ac505c7fbdc415e5ea4dc3b20467bd
rahul-p19 <rahulpandey.dps@gmail.com>
2024-06-15 09:51:08 +0530

    added custom scrollbar and fixed modal ui (except colours)

--- src/components/Conferences.jsx
+++ src/components/Conferences.jsx
@@ -10,6 +10,12 @@ function getData(setInfo) {
 	}, []);
 }
 
+function toggleBlur(){
+    document.querySelector('header').classList.toggle('blur-3xl');
+    document.querySelector('#content').classList.toggle('blur-3xl');
+    document.querySelector('#content').classList.toggle('pointer-events-none');
+}
+
 export default function Conferences() {
 	const [info, setInfo] = useState([]);
     const [showModal,setShowModal] = useState([0,false]);
@@ -17,7 +23,7 @@ export default function Conferences() {
     function Card(item) {
         return (
             <>
-                <div key={item.id} className="flex flex-col items-center my-6 gap-y-1" onClick={()=>{setShowModal([item.id,true])}}>
+                <div key={item.id} className="confCard flex flex-col items-center my-6 gap-y-1" onClick={()=>{setShowModal([item.id,true]);toggleBlur();}}>
                     <img src={item.banner_image} alt={item.title} className="rounded-md" />
                     <h1 className="text-center text-2xl mb-2 dark:text-[#38a9bc]">
                         {item.title}
@@ -35,28 +41,26 @@ export default function Conferences() {
     }
     function Modal({item}) {
         return (
-            <div id='modal' className="fixed z-10 left-12 right-12 top-12 bottom-12 bg-slate-300 rounded-sm shadow-custom">
-                <div key={item.id} className="flex flex-col items-center my-6 gap-y-1">
-                    <button onClick={()=>{setShowModal([0,false])}} className="right-40 fixed">X</button>
-                    <img src={item.banner_image} alt={item.title} className="rounded-md w-[60%]" />
-                    <h1 className="text-center text-5xl mb-2 dark:text-[#38a9bc]">
+            <div id='modal' className="fixed z-10 lg:left-[30%] md:left-[15%] sm:left-[15%] left-[10%] top-6 bottom-6 lg:w-1/3 md:w-2/3 sm:w-2/3 w-4/5 overflow-y-auto bg-slate-300 rounded-sm shadow-custom pb-3">
+                <div key={item.id} className="flex flex-col items-center gap-y-1 relative">
+                    <button onClick={()=>{setShowModal([0,false]);toggleBlur();}} className="right-3 top-3 absolute"><img className="w-6" src="src\assets\iconmonstr-x-mark-circle-lined-32.png" alt="" /></button>
+                    <img src={item.banner_image} alt={item.title} className="w-full" />
+                    <h1 className="text-center text-2xl mb-2 font-semibold dark:text-[#38a9bc]">
                         {item.title}
                     </h1>
-                    <p className="text-xl">{item.description}</p>
-                    <h2 className="text-lg dark:text-[#38a9bc]">
-                        Time: {item.date_time.substring(11,16)}
-                    </h2>
-                    <h2 className="text-lg dark:text-[#38a9bc]">
-                        Date: {item.date_time.substring(8, 10)}/
+                    <p className="text-lg mx-4 mb-2">{item.description}</p>
+                    <h2 className="text-lg mb-2 dark:text-[#38a9bc]">
+                        <span className="font-semibold">Date: </span> {item.date_time.substring(8, 10)}/
                         {item.date_time.substring(5, 7)}/{item.date_time.substring(0, 4)}
+                        <span className="font-semibold ml-4">Time: </span>{item.date_time.substring(11,16)} 
                     </h2>
-                    <h2 className="text-lg dark:text-[#38a9bc]">
-                        {item.venue_name}, {item.venue_city}, {item.venue_country}
+                    <h2 className="text-lg dark:text-[#8e9495] mb-2 text-center">
+                        <span className="font-semibold">Venue: </span>{item.venue_name}, {item.venue_city}, {item.venue_country}
                     </h2>
-                        <p className="font-semibold">Organised by:</p>
-                    <div className="flex items-center">
-                        <img className="w-10" src={item.organiser_icon} alt={item.organiser_name} />
-                        <p>{item.organiser_name}</p>
+                        <p className="font-semibold text-lg mb-2">Organised by:</p>
+                    <div className="flex items-center justify-center pr-4">
+                        <img className="w-10 ml-3" src={item.organiser_icon} alt='logo'/>
+                        <p className="ml-3 text-lg">{item.organiser_name}</p>
                     </div>
                 </div>
             </div>
